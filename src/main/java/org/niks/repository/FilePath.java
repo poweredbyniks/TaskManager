e0baba6fb1ab0ef213e9576f9109c9f1401de1a1
Nikita Stroganov <stroganovnv@gmail.com>
2021-03-22 12:16:09 +0300

    redundant data removed

--- src/main/java/org/niks/repository/FilePath.java
+++
@@ -1,7 +0,0 @@
-package org.niks.repository;
-
-public class FilePath {
-    public static final String USER_FILE_PATH = "src/main/resources/UsersData.json";
-    public static final String PROJECT_FILE_PATH = "src/main/resources/ProjectsData.json";
-    public static final String TASK_FILE_PATH = "src/main/resources/TasksData.json";
-}
